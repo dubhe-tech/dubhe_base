933a4b641c37e9a9efa58710b7ba71ad0ef247c2
zzp <zhuzhongpei90@163.com>
2017-09-27 13:19:14 +0800

    项目初始化

---
+++ src/main/java/com/dubhe/dubhe_base/DubheBaseApplication.java
@@ -0,0 +1,12 @@
+package com.dubhe.dubhe_base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DubheBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DubheBaseApplication.class, args);
+	}
+}
